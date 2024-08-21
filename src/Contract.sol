3c8a896c5945334051048434b915be950b92e084
Brian Le <brian@brianle.xyz>
2024-08-21 16:04:16 -0400

    add VERSION

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+contract Contract {
+    function VERSION() public pure returns (string memory) {
+        return "0.0.1";
+    }
+}
